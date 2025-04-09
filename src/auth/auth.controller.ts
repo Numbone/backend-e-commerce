39777dc592422f67e9fbb65aca977ea7bd9035a9
Nurlybek <nurlybekqwerty@gmail.com>
2025-04-10 03:33:34 +0500

    feat: add login and logout api with save session on redis

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,27 @@
-import { Body, Controller, Get, HttpCode, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Post, Req, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
+import { Request, Response } from 'express';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) { }
   @Post('register')
   @HttpCode(200)
-  public async register(@Body() dto: RegisterDto) {
-    return this.authService.register(dto);
+  public async register(@Req() req: Request, @Body() dto: RegisterDto) {
+    return this.authService.register(req, dto);
+  }
+
+  @Post('login')
+  @HttpCode(200)
+  public async login(@Req() req: Request, @Body() dto: LoginDto) {
+    return this.authService.login(req, dto);
+  }
+
+  @Post('logout')
+  @HttpCode(200)
+  public async logout(@Req() req: Request, @Res() res: Response) {
+    return this.authService.logout(req, res);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,21 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { RegisterDto } from './dto/register.dto';
 import { PrismaService } from '@/prisma/prisma.service';
-import { hash } from 'argon2';
+import { hash, verify } from 'argon2';
 import { AuthMethod, User } from '@prisma/__generated/*';
 import { UserService } from '@/user/user.service';
+import { Request, Response } from 'express';
+import { LoginDto } from './dto/login.dto';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
     public constructor (
         private readonly prismaService: PrismaService,
         private readonly userService: UserService,
+        private readonly configService: ConfigService,
     ){}
-    public async register(dto: RegisterDto){
+    public async register(req:Request,dto: RegisterDto){
         const isExists = await this.userService.findByEmail(dto.email)
         if(isExists){
             throw new NotFoundException('User already exists');
@@ -25,20 +29,55 @@ export class AuthService {
             AuthMethod.CREDENTIALS,
             false
         )
+
+        this.saveSession(req, newUser)
         return {
             message: 'User successfully created'
         }
     }
 
-    public async login(){
-        
+    public async login(req:Request, dto: LoginDto){
+        const user = await this.userService.findByEmail(dto.email)
+        if(!user || !user.password){
+            throw new NotFoundException('User not found or password not found');
+        }
+        const isValidPassword = await verify(user.password, dto.password);
+
+        if(!isValidPassword){
+            throw new NotFoundException('Password is not valid');
+        }
+        return this.saveSession(req, user)
     }
 
-    public async logout(){  
-
-    }
+    public async logout(req:Request,res:Response):Promise<void>{
+        return new Promise((resolve, reject) => {
+            req.session.destroy((err) => {
+                if (err) {
+                    reject(new InternalServerErrorException(
+                        'Internal server error,session could not be destroyed, please try again later'
+                    ));
+                } else {
+                    this.configService.getOrThrow<string>('SESSION_NAME')
+                    resolve();
+                }
+            });
+        })
+    }  
 
     private async saveSession(req:Request, user:User){
-
+        return new Promise((resolve, reject) => {
+            req.session.userId = user.id
+            req.session.save((err) => {
+                if (err) {
+                    reject(new InternalServerErrorException(
+                        'Internal server error,session could not be saved, please try again later'
+                    ));
+                } else {
+                    resolve(
+                        user
+                    );
+                }
+            });
+        });
     }
 }
