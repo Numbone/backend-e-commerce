31f041c692180d3615b21b23b2051d91f47a9266
Nurlybek <nurlybekqwerty@gmail.com>
2025-04-14 03:32:21 +0500

    add module provider google

---
+++ src/config/providers.config.ts
@@ -0,0 +1,29 @@
+import { ConfigService } from '@nestjs/config'
+
+import { TypeOptions } from '@/auth/provider/provider.constants'
+import { GoogleProvider } from '@/auth/provider/services/google.provider'
+
+
+/**
+ * Конфигурация для провайдеров OAuth.
+ *
+ * Эта функция асинхронно извлекает параметры конфигурации из ConfigService
+ * и формирует объект конфигурации для OAuth провайдеров.
+ *
+ * @param configService - Сервис для работы с конфигурацией приложения.
+ * @returns Объект конфигурации для провайдеров OAuth.
+ */
+export const getProvidersConfig = async (
+	configService: ConfigService
+): Promise<TypeOptions> => ({
+	baseUrl: configService.getOrThrow<string>('APPLICATION_URL'),
+	services: [
+		new GoogleProvider({
+			client_id: configService.getOrThrow<string>('GOOGLE_CLIENT_ID'),
+			client_secret: configService.getOrThrow<string>(
+				'GOOGLE_CLIENT_SECRET'
+			),
+			scopes: ['email', 'profile']
+		})
+	]
+})
