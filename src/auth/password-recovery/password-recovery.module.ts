5455de7c3ffeefea798a98f51158f1e86de94b02
Numbone <nurlybekqwerty@gmail.com>
2025-05-18 03:52:33 +0500

    fix: update password-recovery module

--- src/auth/password-recovery/password-recovery.module.ts
+++ src/auth/password-recovery/password-recovery.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PasswordRecoveryService } from './password-recovery.service';
 import { PasswordRecoveryController } from './password-recovery.controller';
+import { UserService } from '@/user/user.service';
+import { MailService } from '@/libs/mail/mail.service';
 
 @Module({
   controllers: [PasswordRecoveryController],
-  providers: [PasswordRecoveryService],
+  providers: [PasswordRecoveryService, UserService, MailService]
 })
-export class PasswordRecoveryModule {}
+export class PasswordRecoveryModule { }
