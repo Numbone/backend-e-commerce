3d7b741946ca9863e9f820180c872cdba9a1968d
Numbone <nurlybekqwerty@gmail.com>
2025-05-22 16:13:52 +0500

    fix: redis port

--- src/main.ts
+++ src/main.ts
@@ -27,6 +27,8 @@ async function bootstrap() {
 	const config = app.get(ConfigService)
 
 	const redis = new IORedis(config.getOrThrow('REDIS_URL'), {
+		host: config.getOrThrow<string>('REDIS_HOST'),
+		port: config.getOrThrow<number>('REDIS_PORT'),
 		password: config.getOrThrow<string>('REDIS_PASSWORD')
 	})
 
